35e79a1840212d71428f549879d10e4107861fae
Christopher Fortenberry <cpfortenberry@gmail.com>
2010-10-02 08:55:26 -0500

    added passwords to suer model and updated show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Christopher Fortenberry"
+  user.email                 "cpfortenberry@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
