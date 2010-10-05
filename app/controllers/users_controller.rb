d2f6e15cc61a81927dc2db3b7cc0a0ebb7c4a62f
Christopher Fortenberry <cpfortenberry@gmail.com>
2010-10-05 14:07:08 -0500

    finished with secion 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def new
     @user = User.new
     @title = "Sign up"
@@ -24,7 +26,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
@@ -38,5 +39,17 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+  
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
   
 end
