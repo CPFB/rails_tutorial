f3f26bb8ba4fd225b59120cec8f28fd9a9176e18
Christopher Fortenberry <cpfortenberry@gmail.com>
2010-10-05 11:46:46 -0500

    changed from cookies to sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:remember_token] = [user.id, user.salt]
     current_user = user
   end
   
@@ -17,18 +17,18 @@ module SessionsHelper
   end
   
   def sign_out
-    cookies.delete(:remember_token)
+    session.delete(:remember_token)
     current_user = nil
   end
   
   private
   
   def user_from_remember_token
-    remember_token ? User.authenticate_with_salt(*remember_token) : nil
+    User.authenticate_with_salt(*remember_token)
   end
   
   def remember_token
-    cookies.signed[:remember_token] || nil
+    session[:remember_token] || [nil, nil]
   end  
   
 end
